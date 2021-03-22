eacca75a9db9524162f64a4a90742869217d6877
quartanicoletta <81098662+quartanicoletta@users.noreply.github.com>
2021-03-22 11:41:39 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,6 +1,10 @@
 # il mio primo codice in R per il telerilevamento!
 setwd("D:/lab/") # Windows
+#install.packages("raster")
 library(raster)
 p224r63_2011 <- brick("p224r63_2011_masked.grd
 p224r63_2011
 plot(p224r63_2011)
+#plottaggio singole bande
+#colour change
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
