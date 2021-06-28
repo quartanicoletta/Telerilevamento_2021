8b0fa429cc2ce8afe3d71b655e5dc5f9d48b7d78
quartanicoletta <81098662+quartanicoletta@users.noreply.github.com>
2021-06-28 11:35:45 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -29,48 +29,49 @@ plot(EN13, col=cls)
 
 #5. Evidenziare le differenze tra i due plot:
 
-ENdif <- EN1 - EN13
+#differenza tra marzo e gennaio
+ENdif <- EN13 - EN1
 
 plot(ENdif, col= cls)
 
 #6. Plottare tutto insieme:
 
 par(mfrow=c(3,1))
-plot(EN1, col=cls, main:"NO2 in January")
-plot(EN13, col=cls, main:"NO2 in March")
-plot(ENdif, col=cls, main:"Difference (January - March)")
+plot(EN1, col=cls, main="NO2 in January")
+plot(EN13, col=cls, main="NO2 in March")
+plot(ENdif, col=cls, main="Difference (March - January)")
 
-#7.  Importare tutte le immagini:
+#7.  Importare tutte le immagini di un intero set:
 
-rlist <- list.files(pattern="EN") #per importare interi set
+rlist <- list.files(pattern="EN") #per importare interi set come lista
 rlist
 
-import <- lapply (rlist,ratser)
-import
+import <- lapply (rlist,raster)
+import #abbiamo tutti i singoli layer importati dalla lista grazie alla funzione raster
 
-EN <- stack(import)
+EN <- stack(import) #abbiamo compattato tutti i file con la funzione stack
 plot (EN, col=cls)
 
 #8. Replicare il plot dell'immagine 1 e 13, usando stack
 
-par (mfrow=c(2,1))
+par (mfrow=c(2,1)) #lego il dataset alle singole bande
 
 plot(EN$EN_0001, col=cls)
 plot(EN$EN_0013, col=cls)
 
-#9. Effettuare una PCA sulle 13 immagni, tramite stack
+#9. Analisi multivariata, effettuare una PCA sulle 13 immagni, tramite stack
 
-ENpca <- rasterPCA(EN)
+ENpca <- rasterPCA(EN) #la funzione dell'intero blocco stack
 summary(ENpca$model)
 
 dev.off()
-plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin") #capiamo che la maggior parte dell'informazione è nel red
 
 #10. calcolare la variabilità nella prima ed ultima immagine, della prima variabile di PCA
 
-PC1sd <- focal(ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+PC1sd <- focal(ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3), fun=sd) #mantengo i nomi delle componenti principali, come per il valore di PC1
 
-plot(PC1sd, col=cls)
+plot(PC1sd, col=cls) #la dev. standard aumenta sulle linee di confine
 
 
 
